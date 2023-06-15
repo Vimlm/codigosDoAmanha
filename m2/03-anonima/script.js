ba1f903fd4169d175a398ee2b778567adae04613
Victor Martins <VictorMLMartins@icloud.com>
2023-06-14 21:26:30 -0300

    feat: anonima

---
+++ m2/03-anonima/script.js
@@ -0,0 +1,20 @@
+//Colocar os meus valores do input em variáveis fixas
+const mensagem = document.querySelector('input');
+const codificacao = document.getElementById('codificacao');
+const codificar = document.getElementById('codificar');
+// const decodificar = document.getElementById('discodificar');
+
+
+if(codificacao.value === 'cesar') {
+  if(codificar == true) {
+    
+  } else {
+
+  }
+} else {
+  if(codificar == true) {
+
+  } else {
+    
+  }
+}
