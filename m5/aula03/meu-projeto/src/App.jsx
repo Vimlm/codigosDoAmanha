47ebad7d8baff0668dfbe9ea33c15fa258ce9be4
Victor Martins <VictorMLMartins@icloud.com>
2023-09-30 20:33:06 -0300

    footer

--- m5/aula03/meu-projeto/src/App.jsx
+++ m5/aula03/meu-projeto/src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
 
 function App() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   )
 }
---
+++ m5/aula03/meu-projeto/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+   <footer className='footer'>
+    <p>Rede Social. Alguns Direitos Reservados</p>
+   </footer>
+  )
+}
+
+export default Footer
